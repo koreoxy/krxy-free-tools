a881370357d88b2f03b04870f6d1dbaba536a86f
unknown <rayssankn@gmail.com>
2026-02-04 21:10:00 +0700

    fix : remove header components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-// ionents/artkit-header";
-import Header from "@/components/Header";
 import Showcase from "@/components/Showcase";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-background">
-      {/* <Header /> */}
       <Showcase />
     </div>
   );
